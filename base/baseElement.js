34753b0fa02f2a788b9b71f02d380f742c64ea35
Nazar Babii <babiyno@gmail.com>
2020-04-28 14:07:25 +0300

    fix

--- base/baseElement.js
+++ base/baseElement.js
@@ -8,9 +8,18 @@ class BaseElement {
         await this.protractorElement.click();
     }
 
+    async hover() {
+        await browser.actions().mouseMove(this.protractorElement).perform();
+    }
+
     async waitForVisible(ms = 15000) {
         let EC = protractor.ExpectedConditions;
         await browser.wait(EC.visibilityOf(this.protractorElement), ms);
+    }
+
+    async waitForPresenceOf(ms = 15000) {
+        let EC = protractor.ExpectedConditions;
+        await browser.wait(EC.presenceOf(this.protractorElement), ms);
     }
 }
 
